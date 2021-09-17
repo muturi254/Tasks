4310e28ef151608466dd347978e8971d89793281
muturi254 <petermuturi74@gmail.com>
2021-09-17 16:35:31 +0300

    set up firebase for project
    create login and sign up UI

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyD1FGDiPru-xpI1lHTwV1rnT0Im1o5ApLo",
+    authDomain: "tasks-11571.firebaseapp.com",
+    projectId: "tasks-11571",
+    storageBucket: "tasks-11571.appspot.com",
+    messagingSenderId: "918611746165",
+    appId: "1:918611746165:web:c0d3ca44e64f9f8ce0073c",
+    measurementId: "G-3NZCFPW3L0"
+  }
 };
 
 /*
