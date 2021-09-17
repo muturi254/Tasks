b555e7eeae772619014da94c2b52622bc004d12e
muturi254 <petermuturi74@gmail.com>
2021-09-17 17:15:49 +0300

    make style consistent in login and sign up component

--- src/app/angular-routing.module.ts
+++ src/app/angular-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 
+const routes: Routes = [
+  {path:'login', component: LoginComponent},
+  {path:'signup', component: SignupComponent}
+]
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
 export class AngularRoutingModule { }
